9eb57860116c458db8ae4d975a27b49bbef94704
Brian Camps Martínez <camps.martinez.brian@alumnat.copernic.cat>
2022-10-18 19:40:48 +0200

    Desarrollo de activities: -Desaroollo de 3 activity -Estilos de botones

---
+++ app/src/main/java/com/example/schoolquest/Login_Alumne.kt
@@ -0,0 +1,11 @@
+package com.example.schoolquest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Login_Alumne : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_alumne)
+    }
+}
---
+++ app/src/main/java/com/example/schoolquest/login_professor.kt
@@ -0,0 +1,11 @@
+package com.example.schoolquest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class login_professor : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_professor)
+    }
+}
