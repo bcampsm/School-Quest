5db130a8a420555d4869905c0befe3ca4cfa0743
Aitor Pérez Romero <perez.romero.aitor@alumnat.copernic.cat>
2022-10-18 17:55:21 +0200

    Implementada splashscreen -També logo de la app

--- app/src/main/java/com/example/schoolquest/MainActivity.kt
+++ app/src/main/java/com/example/schoolquest/MainActivity.kt
@@ -2,9 +2,12 @@ package com.example.schoolquest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.schoolquest.R.style.Theme_SchoolQuest
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(Theme_SchoolQuest)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
