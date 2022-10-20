cb067e1ae021eacd31f2968787ba0fae6829014d
Aitor Pérez Romero <perez.romero.aitor@alumnat.copernic.cat>
2022-10-20 17:07:42 +0200

    Retocs i revisions  -Esborrades strings repetides  -Solucionats warnings i optimitzacions visuals

--- app/src/main/java/com/example/schoolquest/Login_Alumne.kt
+++ app/src/main/java/com/example/schoolquest/Login_Alumne.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 
 class Login_Alumne : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        setTheme(R.style.Theme_SchoolQuest)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_alumne)
     }
