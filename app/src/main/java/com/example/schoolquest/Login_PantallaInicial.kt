33569632590b5e86c89244fea09578622e4b6ac2
Aitor Pérez Romero <perez.romero.aitor@alumnat.copernic.cat>
2022-10-24 20:59:52 +0200

    Afegida navigacio de la pantalla inicial a les pantalles de login

--- app/src/main/java/com/example/schoolquest/Login_PantallaInicial.kt
+++ app/src/main/java/com/example/schoolquest/Login_PantallaInicial.kt
@@ -1,7 +1,9 @@
 package com.example.schoolquest
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.example.schoolquest.R.style.Theme_SchoolQuest
 
 class Login_PantallaInicial : AppCompatActivity() {
@@ -10,5 +12,19 @@ class Login_PantallaInicial : AppCompatActivity() {
         supportActionBar?.hide()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_pantalla_inicial)
+
+        //Botó per obrir Login_Alumne.kt
+        val buttonAlumne = findViewById<Button>(R.id.ButtonLogin1)
+        buttonAlumne.setOnClickListener {
+            val intent = Intent(this, Login_Alumne::class.java)
+            startActivity(intent)
+        }
+
+        //Botó per obrir login_professor.kt
+        val buttonProfessor = findViewById<Button>(R.id.ButtonLogin2)
+        buttonProfessor.setOnClickListener {
+            val intent = Intent(this, Login_Alumne::class.java)
+            startActivity(intent)
+        }
     }
 }
