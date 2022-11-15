7d66cd19896cebbff39c3ce5bb6e7fab2b445ab7
Brian Camps Martínez <camps.martinez.brian@alumnat.copernic.cat>
2022-11-15 19:44:02 +0100

    Menu lateral Professor: -Correcio d'errors. -Menu funcional

--- app/src/main/java/com/example/schoolquest/MainActivity2.kt
+++ app/src/main/java/com/example/schoolquest/MainActivity2.kt
@@ -5,25 +5,25 @@ import android.os.Bundle
 import android.view.MenuItem
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.core.view.GravityCompat
-import com.example.schoolquest.databinding.ActivityMainBinding
+import com.example.schoolquest.databinding.ActivityMain2Binding
 import com.google.android.material.snackbar.Snackbar
 
 class MainActivity2 : AppCompatActivity() {
     private lateinit var toogle: ActionBarDrawerToggle
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMain2Binding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivityMainBinding.inflate(layoutInflater)
+        val binding = ActivityMain2Binding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        toogle = ActionBarDrawerToggle(this, binding.drawerLayout, R.string.openDrawer, R.string.CloseDrawer )
-        binding.drawerLayout.addDrawerListener(toogle)
+        toogle = ActionBarDrawerToggle(this, binding.drawerLayout2, R.string.openDrawer, R.string.CloseDrawer )
+        binding.drawerLayout2.addDrawerListener(toogle)
         toogle.syncState()
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
-        binding.navigationView.setNavigationItemSelectedListener {
+        binding.navigationView2.setNavigationItemSelectedListener {
 
             when(it.itemId){
 
@@ -73,7 +73,7 @@ class MainActivity2 : AppCompatActivity() {
                     ).show()
                 }
             }
-            binding.drawerLayout.openDrawer(GravityCompat.START)
+            binding.drawerLayout2.openDrawer(GravityCompat.START)
             true
         }
     }
