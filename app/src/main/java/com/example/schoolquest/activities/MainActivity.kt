1cf8e8bd43714a20caa6842add2dbaf20dbcd1f1
camps.martinez.brian <BrianCamps1998>
2022-11-16 20:28:26 +0100

    NavGraph + NavigationComponent

--- app/src/main/java/com/example/schoolquest/activities/MainActivity.kt
+++ app/src/main/java/com/example/schoolquest/activities/MainActivity.kt
@@ -7,7 +7,7 @@ import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.core.view.GravityCompat
 import com.example.schoolquest.R
 import com.example.schoolquest.databinding.ActivityMainBinding
-import com.example.schoolquest.fragments.Tasques_Alumne
+import com.example.schoolquest.fragments.*
 import com.google.android.material.snackbar.Snackbar
 
 
@@ -15,6 +15,11 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var toogle: ActionBarDrawerToggle
     private lateinit var binding: ActivityMainBinding
+
+    //Navigation Component
+    val fragmentManager = supportFragmentManager
+    val fragmentTransaction = supportFragmentManager.beginTransaction()
+    private lateinit var fragment: androidx.fragment.app.Fragment
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,52 +35,31 @@ class MainActivity : AppCompatActivity() {
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
+        //Navigation Component
+        fragment = Tasques_Alumne()
+        fragmentTransaction.replace(R.id.fragmentContainerView, fragment)
+        fragmentTransaction.commit()
+        //
+
         binding.navigationView.setNavigationItemSelectedListener {
 
             when(it.itemId){
 
-                R.id.menuPerfil -> {
+                R.id.menuPerfil -> fragment = Perfil_Alumne()
+
+                R.id.menuTasques -> fragment = Tasques_Alumne()
+
+                R.id.menuCalendari -> fragment = Calendari_alumne()
+
+                R.id.menuBotigaDeRecompenses -> fragment = Tenda_Alumnes()
+
+                R.id.menuSettings -> fragment = Configuracio()
+
+                R.id.logOut -> {
 
                     Snackbar.make(
                         binding.root,
-                        "menuPerfil",
-                        Snackbar.LENGTH_SHORT
-                    ).show()
-                }
-
-                R.id.menuTasques -> {
-
-                    Snackbar.make(
-                        binding.root,
-                        "menuTasques",
-                        Snackbar.LENGTH_SHORT
-                    ).show()
-                }
-
-                R.id.menuCalendari -> {
-
-                    Snackbar.make(
-                        binding.root,
-                        "menuCalendari",
-                        Snackbar.LENGTH_SHORT
-                    ).show()
-                }
-
-                R.id.menuBotigaDeRecompenses -> {
-
-                    Snackbar.make(
-                        binding.root,
-                        "menuBotigaDeRecompenses",
-                        Snackbar.LENGTH_SHORT
-                    )
-                        .show()
-                }
-
-                R.id.menuSettings -> {
-
-                    Snackbar.make(
-                        binding.root,
-                        "menuSettings",
+                        "LOG OUT",
                         Snackbar.LENGTH_SHORT
                     ).show()
                 }
@@ -84,12 +68,13 @@ class MainActivity : AppCompatActivity() {
                 true
             }
 
-        val fragmentManager = supportFragmentManager
+        //Navigation Component
         val transaction = fragmentManager.beginTransaction()
-        transaction.replace(R.id.fragmentContainerView2, Tasques_Alumne())
+        transaction.replace(R.id.fragmentContainerView, Tasques_Alumne())
         transaction.addToBackStack(null)
         transaction.commit()
         }
+        //
 
         override fun onOptionsItemSelected(item: MenuItem): Boolean {
             if (toogle.onOptionsItemSelected(item)){
