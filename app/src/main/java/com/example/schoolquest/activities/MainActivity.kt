842932189bdb737f98b9370b62bebfdc10f97f9a
Brian Camps Martínez <camps.martinez.brian@alumnat.copernic.cat>
2022-12-05 21:33:31 +0100

    Navegacio cambiada a .navigate

--- app/src/main/java/com/example/schoolquest/activities/MainActivity.kt
+++ app/src/main/java/com/example/schoolquest/activities/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.example.schoolquest.activities
 
 import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.core.view.GravityCompat
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.NavHostFragment
 import com.example.schoolquest.R
 import com.example.schoolquest.databinding.ActivityMainBinding
 import com.example.schoolquest.fragments.*
@@ -25,11 +29,13 @@ class MainActivity : AppCompatActivity() {
     //Navigation Component
     val fragmentManager = supportFragmentManager
     lateinit var fragment: Fragment
+    lateinit var navHostFragment: NavHostFragment
+    lateinit var navController: NavController
     //
 
     override fun onCreate(savedInstanceState: Bundle?) {//onCreate
         super.onCreate(savedInstanceState)
-        val binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         miMainActivity = this
@@ -44,46 +50,56 @@ class MainActivity : AppCompatActivity() {
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
-        //Navigation Component
-        fragment = Tasques_Alumne()// A la variable "fragment" la igualem al fragment que primerament volem mostrar
-        var fragmentTransaction = supportFragmentManager.beginTransaction()
-        fragmentTransaction.replace(R.id.fragmentContainerView, fragment)//fem un remplaç del fragment actual que es mostra al fragment container view, per el que hem associat previament a la variable fragment.
-        fragmentTransaction.addToBackStack(null)//afegim els fragments a darrere
-        fragmentTransaction.commit()
-        //
+//        //Navigation Component Antiguo
+//        fragment = Tasques_Alumne()// A la variable "fragment" la igualem al fragment que primerament volem mostrar
+//        var fragmentTransaction = supportFragmentManager.beginTransaction()
+//        fragmentTransaction.replace(R.id.fragmentContainerView, fragment)//fem un remplaç del fragment actual que es mostra al fragment container view, per el que hem associat previament a la variable fragment.
+//        fragmentTransaction.addToBackStack(null)//afegim els fragments a darrere
+//        fragmentTransaction.commit()
+//        //
 
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHostFragment.navController
+
+        //Listener del menu lateral
         binding.navigationView.setNavigationItemSelectedListener {//Menu lateral, per a cada click en cadscun dels items, igualem la variable fragment amb el fragment que l'usuari selecciona amb el menu
 
             when (it.itemId) {
 
-                R.id.menuPerfil -> fragment = Perfil_Alumne()
+                R.id.menuPerfil -> {
+                    Navigation.findNavController(binding.fragmentContainerView).navigate(R.id.action_global_perfil_Alumne)
+                }
 
-                R.id.menuTasques -> fragment = Tasques_Alumne()
+                R.id.menuTasques -> {
+                    Navigation.findNavController(binding.fragmentContainerView).navigate(R.id.action_global_tasques_Alumne)
+                }
 
-                R.id.menuCalendari -> fragment = Calendari_alumne()
+                R.id.menuCalendari -> {
+                    Navigation.findNavController(binding.fragmentContainerView).navigate(R.id.action_global_calendari_alumne)
+                }
 
-                R.id.menuBotigaDeRecompenses -> fragment = Tenda_Alumnes()
+                R.id.menuBotigaDeRecompenses -> {
+                    Navigation.findNavController(binding.fragmentContainerView).navigate(R.id.action_global_tenda_Alumnes)
+                }
 
-                R.id.menuSettings -> fragment = Configuracio()
+                R.id.menuSettings -> {
+                    Navigation.findNavController(binding.fragmentContainerView).navigate(R.id.action_global_configuracio2)
+                }
 
-                R.id.logOut -> {//log out no implementt, nomes mostra un snackbar
-
-                    Snackbar.make(
-                        binding.root,
-                        "LOG OUT",
-                        Snackbar.LENGTH_SHORT
-                    ).show()
+                R.id.logOut -> {
+                    val intent = Intent(this, Login_PantallaInicial::class.java)
+                    startActivity(intent)
                 }
             }
             binding.drawerLayout.openDrawer(GravityCompat.START)
-            binding.navigationView.postDelayed(
-                {
-                    val transaction = fragmentManager.beginTransaction()
-                    transaction.replace(R.id.fragmentContainerView, fragment)
-                    transaction.addToBackStack(null)
-                    transaction.commit()
-                }, 275
-            )
+//            binding.navigationView.postDelayed(
+//                {
+//                    val transaction = fragmentManager.beginTransaction()
+//                    transaction.replace(R.id.fragmentContainerView, fragment)
+//                    transaction.addToBackStack(null)
+//                    transaction.commit()
+//                }, 275
+//            )
             //Navigation Component
 
             binding.drawerLayout.close()
--- app/src/main/java/com/example/schoolquest/activities/MainActivity2.kt
+++ app/src/main/java/com/example/schoolquest/activities/MainActivity2.kt
@@ -1,11 +1,17 @@
 package com.example.schoolquest.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.core.view.GravityCompat
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.NavGraph
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import com.example.schoolquest.R
 import com.example.schoolquest.databinding.ActivityMain2Binding
 import com.example.schoolquest.fragments.*
@@ -22,12 +28,14 @@ class MainActivity2 : AppCompatActivity() {
 
     //Navigation Component
     val fragmentManager = supportFragmentManager
-    lateinit var fragment: Fragment
+    lateinit var navHostFragment: NavHostFragment
+    lateinit var navController: NavController
+    private lateinit var navGraph: NavGraph
     //
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivityMain2Binding.inflate(layoutInflater)
+        binding = ActivityMain2Binding.inflate(layoutInflater)
         setContentView(binding.root)
 
         miMainActivity2 = this
@@ -42,53 +50,79 @@ class MainActivity2 : AppCompatActivity() {
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
-        //Navigation Component
-        fragment = Tasques_Professor()// A la variable "fragment" la igualem al fragment que primerament volem mostrar
-        var fragmentTransaction = supportFragmentManager.beginTransaction()
-        fragmentTransaction.replace(R.id.fragmentContainerView, fragment)//fem un remplaç del fragment actual que es mostra al fragment container view, per el que hem associat previament a la variable fragment.
-        fragmentTransaction.addToBackStack(null)//afegim els fragments a darrere
-        fragmentTransaction.commit()
-        //
+//        //Navigation Component
+//        fragment = Tasques_Professor()// A la variable "fragment" la igualem al fragment que primerament volem mostrar
+//        var fragmentTransaction = supportFragmentManager.beginTransaction()
+//        fragmentTransaction.replace(R.id.fragmentContainerView, fragment)//fem un remplaç del fragment actual que es mostra al fragment container view, per el que hem associat previament a la variable fragment.
+//        fragmentTransaction.addToBackStack(null)//afegim els fragments a darrere
+//        fragmentTransaction.commit()
+//        //
+
+        val navHostFragment=supportFragmentManager.findFragmentById(R.id.fragmentContainerView2) as NavHostFragment
+
+        val graphInflater = navHostFragment.navController.navInflater//
+        //navGraph = graphInflater.inflate(R.navigation.nav_graph)//
+
+       // navGraph=findNavController(R.id.tasques_Professor)
+        navController = navHostFragment.navController
+
+        //navGraph.setStartDestination(R.id.tasques_Professor)
+
+
 
         binding.navigationView2.setNavigationItemSelectedListener {
 
             when (it.itemId) {
 
-                R.id.menuPerfil2 -> fragment = Perfil_Professor()
+                R.id.menuPerfil2 -> {
+                    Navigation.findNavController(binding.fragmentContainerView2).navigate(R.id.action_global_perfil_Professor)
+                }
 
-                R.id.menuTasques2 -> fragment = Tasques_Professor()
+                R.id.menuTasques2 -> {
+                    Navigation.findNavController(binding.fragmentContainerView2).navigate(R.id.action_global_tasques_Professor)
+                }
 
-                R.id.menuCalendari2 -> fragment = Calendari_professor()
+                R.id.menuCalendari2 -> {
+                    Navigation.findNavController(binding.fragmentContainerView2).navigate(R.id.action_global_calendari_professor)
+                }
 
-                R.id.menuBotigaDeRecompenses2 -> fragment = Tenda_Professor()
+                R.id.menuBotigaDeRecompenses2 -> {
+                    Navigation.findNavController(binding.fragmentContainerView2).navigate(R.id.action_global_tenda_Professor)
+                }
 
-                R.id.menuSettings2 -> fragment = Configuracio()
+                R.id.menuSettings2 -> {
+                    Navigation.findNavController(binding.fragmentContainerView2).navigate(R.id.action_global_configuracio2)
+                }
 
-                R.id.logOut2 -> {//log out no implementt, nomes mostra un snackbar
-
-                    Snackbar.make(
-                        binding.root,
-                        "LOG OUT",
-                        Snackbar.LENGTH_SHORT
-                    ).show()
+                R.id.logOut2 -> {
+                    val intent = Intent(this, Login_PantallaInicial::class.java)
+                    startActivity(intent)
                 }
             }
             binding.drawerLayout2.openDrawer(GravityCompat.START)
 
-            binding.navigationView2.postDelayed(
-                {
-                    val transaction = fragmentManager.beginTransaction()
-                    transaction.replace(R.id.fragmentContainerView, fragment)
-                    transaction.addToBackStack(null)
-                    transaction.commit()
-                }, 275
-            )
+//            binding.navigationView2.postDelayed(
+//                {
+//                    val transaction = fragmentManager.beginTransaction()
+//                    transaction.replace(R.id.fragmentContainerView, fragment)
+//                    transaction.addToBackStack(null)
+//                    transaction.commit()
+//                }, 275
+//            )
             //Navigation Component
 
             binding.drawerLayout2.close()
 
+
+
             true
         }
+
+        binding.navigationView2.postDelayed({
+            Navigation.findNavController(binding.fragmentContainerView2).navigate(R.id.action_global_tasques_Professor)
+        }, 1)
+
+       // Navigation.findNavController(binding.fragmentContainerView2).navigate(R.id.action_global_tasques_Professor)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
