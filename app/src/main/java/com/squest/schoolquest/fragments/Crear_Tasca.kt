7362dda7e153f7f30619d58dde87a6d5ae2d949a
Brian Camps Martínez <camps.martinez.brian@alumnat.copernic.cat>
2023-01-18 19:44:36 +0100

    Login default

--- app/src/main/java/com/squest/schoolquest/fragments/Crear_Tasca.kt
+++ app/src/main/java/com/squest/schoolquest/fragments/Crear_Tasca.kt
@@ -5,12 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.firebase.firestore.FirebaseFirestore
 import com.squest.schoolquest.R
+import com.squest.schoolquest.databinding.FragmentCrearTascaBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
+
 
 /**
  * A simple [Fragment] subclass.
@@ -18,9 +21,11 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class Crear_Tasca : Fragment() {
+
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    private var _binding : FragmentCrearTascaBinding? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,12 +35,33 @@ class Crear_Tasca : Fragment() {
         }
     }
 
+    private val db = FirebaseFirestore.getInstance()
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_crear__tasca, container, false)
+        _binding = FragmentCrearTascaBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.buttonCrearTasca.setOnClickListener {
+
+//            db.collection("Tasks").document(auth.uid.toString()).set(
+//                hashMapOf("User" to binding.inputEmailP.text.toString(),
+//                    "Name" to binding.inputNameP.text.toString(),
+//                    "Surname1" to binding.inputSurname1P.text.toString(),
+//                    "Surname2" to binding.inputSurname2P.text.toString())
+//            )
+
+        }
+
     }
 
     companion object {
